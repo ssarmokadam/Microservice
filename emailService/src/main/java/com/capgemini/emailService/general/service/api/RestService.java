618792f4abe45031cb6a8ae6e35490810f86c385
Sarmokadam <sneha.sarmokadam@capgemini.com>
2017-11-17 13:04:45 +0530

    deleting old emailservice

--- emailService/src/main/java/com/capgemini/emailService/general/service/api/RestService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.emailService.general.service.api;
-
-/**
- * A marker interface for REST services. REST service interfaces of components should extend this interface.
- *
- */
-public interface RestService {
-
-}
